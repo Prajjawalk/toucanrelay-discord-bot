b2a5f1e620e533f3a833817d674ca6d533ddb11b
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-04-27 13:11:55 +0530

    reverted patch

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import express from 'express';
 import {
   InteractionType,
   InteractionResponseType,
-  verifyKeyMiddleware
 } from 'discord-interactions';
 import { DiscordRequest, VerifyDiscordRequest, getRandomEmoji } from './utils.js';
 import timeout from 'connect-timeout';
@@ -13,11 +12,11 @@ const app = express();
 // Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
 // Parse request body and verifies incoming requests using discord-interactions package
-app.use(express.json());
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
-app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
+app.post('/interactions', async function (req, res) {
   // Interaction type and data
   const { type, id, data } = req.body;
 
