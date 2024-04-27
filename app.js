d3026429fa4763eb5d5495a9b0237da0de3fe196
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-04-27 13:04:32 +0530

    add verify middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import {
   InteractionType,
   InteractionResponseType,
+  verifyKeyMiddleware
 } from 'discord-interactions';
 import { DiscordRequest, VerifyDiscordRequest, getRandomEmoji } from './utils.js';
 import timeout from 'connect-timeout';
@@ -12,11 +13,11 @@ const app = express();
 // Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
 // Parse request body and verifies incoming requests using discord-interactions package
-app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+app.use(express.json());
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
-app.post('/interactions', async function (req, res) {
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
   // Interaction type and data
   const { type, id, data } = req.body;
 
